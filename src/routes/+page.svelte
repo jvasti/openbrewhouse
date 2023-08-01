b963eec17c4035c6ac24de4f1a879c7430bf21bc
Juha Västi <juha@vasti.fi>
2023-08-01 20:30:58 +0300

    Add ipsum test paragraphs to test out deployment action

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,47 @@
 
 <section>
 	<h1>Openbrewhouse WIP</h1>
+	<p>
+		Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an
+		unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level.
+		That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex
+		corresponding to an REM state. The engineering section's critical. Destruction is imminent.
+		Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.
+	</p>
+
+	<p>
+		Exceeding reaction chamber thermal limit. We have begun power-supply calibration. Force fields
+		have been established on all turbo lifts and crawlways. Computer, run a level-two diagnostic on
+		warp-drive systems. Antimatter containment positive. Warp drive within normal parameters. I read
+		an ion trail characteristic of a freighter escape pod. The bomb had a molecular-decay detonator.
+		Detecting some unusual fluctuations in subspace frequencies.
+	</p>
+
+	<p>
+		Run a manual sweep of anomalous airborne or electromagnetic readings. Radiation levels in our
+		atmosphere have increased by 3,000 percent. Electromagnetic and subspace wave fronts approaching
+		synchronization. What is the strength of the ship's deflector shields at maximum output? The
+		wormhole's size and short period would make this a local phenomenon. Do you have sufficient data
+		to compile a holographic simulation?
+	</p>
+
+	<p>
+		Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant
+		failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust
+		shield harmonics to the upper EM band when proceeding. These appear to be some kind of
+		power-wave-guide conduits which allow them to work collectively as they perform ship functions.
+		Increase deflector modulation to upper frequency band.
+	</p>
 </section>
 
 <style>
 	h1 {
 		text-align: center;
+	}
+	p {
+		width: 400px;
+		margin: auto;
+		margin: 0 auto;
+		padding-bottom: 16px;
 	}
 </style>
