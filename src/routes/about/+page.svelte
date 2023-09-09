4202d3989b3a894e44e22f316b4a2e42098f6bf7
Juha Västi <juha@vasti.fi>
2023-09-09 16:45:05 +0300

    Add stub /about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="Openbrewhouse" />
+</svelte:head>
+
+<div class="container mx-auto p-8 space-y-8 text-justify">
+	<p>About page stub</p>
+</div>
