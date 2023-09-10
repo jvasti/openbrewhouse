f15649de71fee82cc9e79071fe14d382dda21abb
Juha Västi <juha@vasti.fi>
2023-09-10 16:44:36 +0300

    Separate components, add navbar collapsing on mobile + navigation drawer

---
+++ src/lib/AppBar/AppBar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+	function drawerOpen(): void {
+		drawerStore.open();
+	}
+</script>
+
+<AppBar>
+	<svelte:fragment slot="lead">
+		<div class="flex items-center">
+			<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+				<span>
+					<svg viewBox="0 0 100 75" class="fill-token w-5 h-5">
+						<rect width="100" height="20" />
+						<rect y="30" width="100" height="20" />
+						<rect y="60" width="100" height="20" />
+					</svg>
+				</span>
+			</button>
+			<strong class="text-xl uppercase">Openbrewhouse</strong>
+		</div>
+	</svelte:fragment>
+</AppBar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,22 @@
 <script>
 	import '../app.postcss';
 	import { AppShell } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/Navigation/Navigation.svelte';
+	import Drawer from '$lib/Drawer/Drawer.svelte';
+	import AppBar from '$lib/AppBar/AppBar.svelte';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+
+	initializeStores();
 </script>
 
-<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+<Drawer />
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-56">
+	<svelte:fragment slot="header">
+		<AppBar />
+	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
-		<!-- Insert the list: -->
-		<nav class="list-nav">
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/about">About</a></li>
-			</ul>
-		</nav>
-		<!-- --- -->
+		<Navigation />
 	</svelte:fragment>
 	<slot />
 </AppShell>
