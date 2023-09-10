d67b0f940aebf98021d284f960e94b3d4410c682
Juha Västi <juha@vasti.fi>
2023-09-10 16:44:36 +0300

    Separate components, add navbar collapsing on mobile + navigation drawer

---
+++ src/lib/Drawer/Drawer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Drawer } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/Navigation/Navigation.svelte';
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+</script>
+
+<Drawer>
+	<div class="flex flex-row content-center justify-between h-16 p-4">
+		<h2 class="h2 self-center">Navigation</h2>
+		<button class="btn self-center" on:click={drawerClose}>X</button>
+	</div>
+	<hr />
+	<Navigation />
+</Drawer>
